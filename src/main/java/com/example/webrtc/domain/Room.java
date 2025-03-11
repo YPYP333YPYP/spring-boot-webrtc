1218bb091c867fbcacf9e6a5c0076f77cbec8018
YPYP333YPYP <ypyp333ypyp@gmail.com>
2025-03-11 23:19:45 +0900

    feat: Room record 생성

---
+++ src/main/java/com/example/webrtc/domain/Room.java
@@ -0,0 +1,35 @@
+package com.example.webrtc.domain;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+
+public record Room (
+
+    @Id
+    String id,
+
+    @NotBlank(message = "The room name must be defined")
+    String name,
+
+    @CreatedDate
+    LocalDateTime createdAt,
+
+    @Positive(message = "The room participant limit must be greater than zero")
+    int participantLimit,
+
+    boolean isActive
+
+) {
+    public static Room of(
+        String name, int participantLimit, boolean isActive
+    ) {
+        return new Room(
+            UUID.randomUUID().toString(), name, null, participantLimit, isActive
+        );
+    }
+}
