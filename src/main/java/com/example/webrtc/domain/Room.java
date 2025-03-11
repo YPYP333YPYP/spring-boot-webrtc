7d35fd18b0f1c4f93a11a40a14801e916844b203
YPYP333YPYP <ypyp333ypyp@gmail.com>
2025-03-12 00:57:29 +0900

    refactor: Table 생성 로직 변경 <엔티티 생성 시 ID를 부여하는 것이 아닌 DB 테이블 생성 시 ID 부여>

--- src/main/java/com/example/webrtc/domain/Room.java
+++ src/main/java/com/example/webrtc/domain/Room.java
@@ -1,22 +1,20 @@
 package com.example.webrtc.domain;
 
 
+import org.springframework.data.annotation.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import java.time.LocalDateTime;
 import java.util.UUID;
-import lombok.Data;
-import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 
 public record Room (
-
+    @Id
     String id,
 
     @NotBlank(message = "The room name must be defined")
     String name,
 
-    @CreatedDate
     LocalDateTime createdAt,
 
     @Positive(message = "The room participant limit must be greater than zero")
@@ -29,7 +27,7 @@ public record Room (
         String name, int participantLimit, boolean isActive
     ) {
         return new Room(
-            UUID.randomUUID().toString(), name, null, participantLimit, isActive
+            null, name, LocalDateTime.now(), participantLimit, isActive
         );
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 DROP TABLE IF EXISTS room;
 CREATE TABLE Room (
-                      id VARCHAR(36) PRIMARY KEY,
+                      id VARCHAR(36) DEFAULT uuid_generate_v4()::VARCHAR PRIMARY KEY,
                       name VARCHAR(255) NOT NULL,
                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                       participant_limit INT CHECK (participant_limit > 0) NOT NULL,
