1b6d2f6e96f3fc47a5786d6b08817f2efa05c063
YPYP333YPYP <ypyp333ypyp@gmail.com>
2025-03-11 23:40:28 +0900

    feat: Participant record 작성 및 sql 작성

---
+++ src/main/java/com/example/webrtc/domain/Participant.java
@@ -0,0 +1,28 @@
+package com.example.webrtc.domain;
+
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+
+public record Participant(
+
+    String id,
+
+    @NotBlank(message = "The participant name must be defined")
+    String name,
+
+    @NotBlank(message = "The participant room id must be defined")
+    String roomId,
+
+    LocalDateTime joinedAt,
+
+    boolean hasAudio,
+    boolean hasVideo
+) {
+    public static Participant of(
+        String id, String name, String roomId
+) {
+    return new Participant(id, name, roomId, LocalDateTime.now(), true, true);
+}
+
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE Room (
                       participant_limit INT CHECK (participant_limit > 0) NOT NULL,
                       is_active BOOLEAN NOT NULL
 );
+
+
+DROP TABLE IF EXISTS participant;
+CREATE TABLE participant (
+                             id VARCHAR(255) PRIMARY KEY,
+                             name VARCHAR(255) NOT NULL,
+                             room_id VARCHAR(255) NOT NULL,
+                             joined_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                             has_audio BOOLEAN NOT NULL DEFAULT true,
+                             has_video BOOLEAN NOT NULL DEFAULT true
+);
